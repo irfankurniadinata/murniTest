16a96e571c0434b9958a6987e694d8f87373997b
irfankurniad <irfan.kurnia.dinata@gmail.com>
2025-05-02 22:04:10 +0700

    add data source

--- app/src/main/java/com/test/murni/data/remote/ArticleDataSource.kt
+++ app/src/main/java/com/test/murni/data/remote/ArticleDataSource.kt
@@ -4,11 +4,14 @@ import com.test.murni.core.BaseResponse
 import com.test.murni.data.model.Article
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface ArticleDataSource {
     @GET("/v4/articles")
     suspend fun getArticle(): Response<BaseResponse<List<Article>>>
 
-    @GET("/v4/articles")
-    suspend fun getArticleDetail(): Response<BaseResponse<List<Article>>>
+    @GET("/v4/articles/{id}")
+    suspend fun getArticleDetail(
+        @Path("id") id: String?
+    ): Response<BaseResponse<Article>>
 }
